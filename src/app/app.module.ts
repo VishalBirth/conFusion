5893b1bf7f7fdba41e534bae2d2a40a5a35d720b
Vishal Kumar <vishalkumar@folio3.com>
2017-09-22 13:37:40 +0500

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from '@angular/material'; 
-import { FlexLayoutModule } from '@angular/flex-layout';
-
-
 import 'hammerjs';
+
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
 import { DishService } from './services/dish.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent,
-    DishDetailComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    FlexLayoutModule
-  ],
-  providers: [
-    DishService
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent, MenuComponent, DishDetailComponent, HeaderComponent, FooterComponent
+    ],
+    imports: [
+        BrowserModule, BrowserAnimationsModule, MaterialModule, FlexLayoutModule
+    ],
+    providers: [DishService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
